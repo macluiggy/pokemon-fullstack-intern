8afb4fda72315970fbb666d698086b76ab7dd3c6
luiggy macias <ferrinluiggy@gmail.com>
2022-01-06 14:46:18 -0500

    making the login route and creating the jwtMethod to generate a token for every user id

--- backend/src/routes/jwtAauth.route.ts
+++ backend/src/routes/jwtAauth.route.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import bcrypt from "bcrypt";
 import pool from "../db";
 const router = Router();
+import jwtGenerator from "../utils/jwtGenerator";
 
 router.route("/register").post(async (req, res) => {
   try {
@@ -23,12 +24,37 @@ router.route("/register").post(async (req, res) => {
       "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
       [name, email, bcryptPassword]
     );
-    res.json(newUser.rows[0]);
+    // res.json(newUser.rows[0]);
     //5. generating our jwt token
+    const token = jwtGenerator(newUser.rows[0].user_id);
+    res.json({ token });
   } catch (error) {
     trace(error);
     res.status(500).send("Server error");
   }
 });
 
+router.route("/login").post(async (req, res) => {
+  try {
+    // 1. desctructure the req.body
+    const { email, password } = req.body;
+    //2 check if the user exists if not return throw error
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+    if (!user.rows[0])
+      return res.status(401).send("Password or email is incorrect"); // 401 means unthenticated
+    // 3. check if the password is correct, the password is the same as the one in the database
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    ); // compare is a method of bcrypt that takes the password and the hash and returns true or false depending on if the password is correct
+    if (!validPassword) res.status(401).send("Password or email is incorrect");
+    //4. give them the jwt token
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
+  } catch (error: any) {
+    console.error(error.message);
+  }
+});
 export default router;
---
+++ backend/src/utils/jwtGenerator.ts
@@ -0,0 +1,13 @@
+import jwt, { Secret } from "jsonwebtoken";
+require("dotenv").config();
+
+function jwtGenerator(user_id) {
+  const payload = {
+    user: user_id,
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET as Secret, {
+    expiresIn: "1hr",
+  }); // jsw sign is a method of jwt that takes payload and secret and returns a token
+}
+
+export default jwtGenerator;
