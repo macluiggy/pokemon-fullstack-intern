ca1ea581c7d75b168f76bfdab8ad8c1a2198252f
luiggy macias <ferrinluiggy@gmail.com>
2022-01-06 10:45:47 -0500

    addind notes

---
+++ backend/notes.js
@@ -0,0 +1,14 @@
+/*
+differences between authentication and authorization:
+authentication:
+authentication is the process of verifying the identity of a user. In other words, it is the process of verifying that the user is who he says he is.
+example:
+are you luiggy?
+
+authorization:
+authorization is the process of verifying that the user is allowed to perform a certain action. In other, depending on the user credentials, the user is allowed to perform a certain action.
+example:
+are you allowed to delete this note?
+
+The reason to use JWT is to prevent the user from being able to impersonate another user. In other words, the person is really who he says he is or is not a robot.
+*/
