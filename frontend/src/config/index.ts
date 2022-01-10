0ad4ab2fd85b47b2324398ab9e7e2c84b57c1d1f
luiggy macias <ferrinluiggy@gmail.com>
2022-01-10 11:51:47 -0500

    changing the backend proxy to production

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -2,6 +2,6 @@
 // config();
 
 // export const isProduction = process.env.NODE_ENV === "production";
-export const proxy = false
+export const proxy = !false
   ? "https://postgres-node-login.herokuapp.com"
   : "http://localhost:3000";
