d2934e3c050b15810891f9ca3f8b49d063a1dadb
luiggy macias <ferrinluiggy@gmail.com>
2022-01-10 08:13:29 -0500

    adding dotenv to config folder

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,3 +1,6 @@
+import { config } from "dotenv";
+config();
+
 export const isProduction = process.env.NODE_ENV === "production";
 export const proxy = isProduction
   ? "https://postgres-node-login.herokuapp.com"
